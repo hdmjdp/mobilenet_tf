6bce7e01e09f2a7082a37f3d9ac49b27f9849da5
hdmjdp <hdmjdp@163.com>
2017-11-28 12:19:00 +0800

    Update mobilenet.py

--- mobilenet.py
+++ mobilenet.py
@@ -9,7 +9,7 @@ def depth_point_conv2d(X, n_ch_in, n_ch_out, kernel_size, strides, name=None,pad
                         initializer=tf.random_normal_initializer(stddev=0.1))
     depthwise_conv = tf.nn.depthwise_conv2d(X, depthwise_weights, strides, padding)
     #num_outputs = depth_multiplier * n_ch_in
-    pointwise_shape = [1, 1, n_ch_in, n_ch_out]
+    pointwise_shape = [1, 1, n_ch_in, n_ch_out] #点卷积其实也是普通的二维卷积
     point_weights = tf.get_variable(name=name + 'point_weights',
                         shape=pointwise_shape,
                         dtype=tf.float32,
